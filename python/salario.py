8bab44fdad6c09996959183571f83e214e5f450f
carladagostin <70265939+carladagostin@users.noreply.github.com>
2024-07-03 21:10:37 -0300

    j

--- python/salario.py
+++ python/salario.py
@@ -1,4 +1,1 @@
-salario = float (input(" digite o salario atual:" ))
-aumento = float (input ("porcentagem do aumento"))
-print ("O valor do aumento é:" aumento)
-print ("O valor do novo salario:" aumento)
+print ('hello word')
